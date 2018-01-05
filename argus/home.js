01fea5d52384d9d9de375261471b8782ab123d23
akshatdhabi <akshat.dhabi@gmail.com>
2018-01-05 16:00:44 +0530

    Home Page Finished

---
+++ argus/home.js
@@ -0,0 +1,14 @@
+$(function() {
+  AOS.init();
+  $(".dropdown-button ").dropdown({
+    hover: true
+  });
+  $(".dropdown-button2 ").dropdown({
+    hover: true
+  });
+  $(".button-collapse ").sideNav();
+  $('.parallax ').parallax();
+  $('.slider ').slider();
+
+
+});
