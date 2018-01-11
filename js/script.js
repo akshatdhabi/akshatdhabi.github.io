0236833e44ef2917effa4cdd254d2fe58905163e
akshatdhabi <akshat.dhabi@gmail.com>
2018-01-12 01:45:19 +0530

    mobile kb test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-particlesJS.load('particles-js', 'particlesjs.json', function() {
-  console.log('callback - particles.js config loaded');
+$(function() {
+  particlesJS.load('particle', 'particlesjs.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  function hero() {
+    $("#particle").css({
+      width: jQuery(window).width(),
+      height: (jQuery(window).height() / 4) * 3
+
+    });
+
+  }
+
+  hero();
+
+
+  jQuery(window).resize(function() {
+    var x = jQuery(window).height();
+    if (x <= 300) {
+      return x;
+    } else {
+      hero();
+    }
+  });
+
+
+
+
+
+
 });
